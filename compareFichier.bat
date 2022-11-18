76bd1b43d608f7bb4d7780117762c2c3101bb80f
Floris <Floris.robart11@gmail.com>
2022-11-18 09:58:47 +0100

    ajout de l'expansion des variables

--- compareFichier.bat
+++ compareFichier.bat
@@ -1,78 +1,73 @@
 @echo off
 
+SETLOCAL enabledelayedexpansion
+
 SET "pathFichier1=%~1"
 SET "pathFichier2=%~2"
+SET /a "nbLigne=0"
+
 
 :: Code éxecuter (main) ::
-call :choixFichier1
-call :choixFichier2
+call :choixFichier 1
+call :choixFichier 2
 
-call :selectNbLigneFichiers 1
-call :selectNbLigneFichiers 2
+call :nomFichierTemp 1
+goto :eof
+call :nomFichierTemp 2
+
 
 call :compareFichiers
 
+ENDLOCAL
 :: Met fin au programme
 goto :eof
 
 
 
-:: Vérifie si l'argument 1 est valide ::
-:choixFichier1
-    IF NOT EXIST "%pathFichier1%" (
+:: Vérifie si l'argument est valide ::
+:choixFichier
+    IF NOT EXIST "!pathFichier%~1!" (
         :: Demande à l'utilisateur de choisir un fichier 1 valide ::
-        echo Veuillez choisir le premier fichier à comparer
-        SET /P pathFichier1="Chemin et/ou nom du fichier : "
-        goto :choixFichier1
-    )
-goto :eof
-
-
-:: Vérifie si l'argument 2 est valide ::
-:choixFichier2
-    IF NOT EXIST "%pathFichier2%" (
-        :: Demande à l'utilisateur de choisir un fichier 1 valide ::
-        echo Veuillez choisir le deuxième fichier à comparer
-        SET /P pathFichier2="Chemin et/ou nom du fichier : "
-        goto :choixFichier2
+        IF %~1==1 (
+            SET /P "pathFichier%~1=Veuillez choisir le chemin et/ou le nom du premier fichier à comparer : "
+        ) ELSE (
+            SET /P "pathFichier%~1=Veuillez choisir le chemin et/ou le nom du deuxième fichier à comparer : "
+        )
+        goto :choixFichier %~1
     )
 goto :eof
 
 
 :: Vérifie si le nom du fichier temporaires existe déjà ::
-:selectNbLigneFichiers
+:nomFichierTemp
     :: Compte le nombre de fichier présent dans le dossier ::
     FOR /f %%i IN ('dir "%cd%" /a:-d ^| find "fichier"') DO SET "nbFichier=%%i"
 
     :: Vérifie si le fichier existe déjà ::
     FOR /l %%C IN (1, 1, %nbFichier%) DO (
         IF NOT EXIST temp%%C.txt (
-            SET "nomFichierTemp=temp%%C.txt"
-            goto :nbLigneFichier%~1 %nomFichierTemp%
+            SET "nomFichierTemp%~1=temp%%C.txt"
+            call :nbLigneFichier %~1
+            goto :eof
         )
     )
 goto :eof
 
 
-:: Permet de connaitre le nombre de lignes dans fichier 1 ::
-:nbLigneFichier1
-    SET "nomFichierTemp1=%~1"
-    FIND /v /c "" < %pathFichier1% > %nomFichierTemp1%
-    FOR /f "tokens=1 delims=" %%A IN (%nomFichierTemp%) DO SET /a nbLigne=%%A-1
-goto :eof
-
-:: Permet de connaitre le nombre de lignes dans fichier 2 ::
-:nbLigneFichier2
-    SET "nomFichierTemp2=%~1"
-    FIND /v /c "" < %pathFichier2% > %nomFichierTemp2%
-    FOR /f "tokens=1 delims=" %%A IN (%nomFichierTemp%) DO SET /a nbLigne=%%A-1
+:: Permet de connaitre le nombre de lignes dans les fichiers ::
+:nbLigneFichier
+    echo %~1
+    echo !nomFichierTemp%~1!
+    echo !pathFichier%~1!
+    FIND /v /c "" < "!pathFichier%~1!" > "!nomFichierTemp%~1!"
+    FOR /f "tokens=1 delims=" %%A IN (!nomFichierTemp%~1!) DO SET /a !nbLigne!=%%A-1
 goto :eof
 
 
 :: Boucle pour lire les deux fichiers ligne par ligne ::
 :compareFichiers
     call :verifLigne1
-    FOR /l %%C IN (1, 1, %nbLigne%) DO (
+    FOR /l %%C IN (1, 1, !nbLigne!) DO (
         call :verifFichier %%C
         echo.
     )
