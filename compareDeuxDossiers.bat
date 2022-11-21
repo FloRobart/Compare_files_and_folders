513a8b1e65f0120a627e7dab6eb4ed2eb2d9f9cb
Floris <Floris.robart11@gmail.com>
2022-11-21 21:10:15 +0100

    update

--- compareDeuxDossiers.bat
+++ compareDeuxDossiers.bat
@@ -4,10 +4,80 @@ SETLOCAL enabledelayedexpansion
 SET "pathDossier1=%~1"
 SET "pathDossier2=%~2"
 
-
+::======================::
 :: Code éxecuter (main) ::
-FOR /f %%i IN ('dir "!pathDossier1!" /a /b /o:GEN') DO echo %%i & SET "nbFichier=%%i"
+::======================::
+FOR /l %%i IN (1, 1, 2) DO call :choixDossierSource %%i
+FOR /l %%i IN (1, 1, 2) DO call :nomFichierTemp %%i
+FOR /l %%i IN (1, 1, 2) DO call :nbDossierInSource %%i
+
+echo %nbDossierInSource1% ^| %nbDossierInSource2%
+
+
+
+::call :compareDossiers
 
 ENDLOCAL
 :: Met fin au programme ::
+goto :eof
+
+
+
+::----------------------------------::
+:: Vérifie si l'argument est valide ::
+::----------------------------------::
+:choixDossierSource
+    IF NOT EXIST "!pathDossier%~1!" (
+        :: Demande à l'utilisateur de choisir un dossier 1 valide ::
+        IF %~1==1 (
+            SET /P "pathDossier%~1=Veuillez choisir le chemin et/ou le nom du 1er dossier à comparer : "
+        ) ELSE (
+            SET /P "pathDossier%~1=Veuillez choisir le chemin et/ou le nom du %~1ème dossier à comparer : "
+        )
+        call :choixDossierSource %~1
+    )
+goto :eof
+
+
+
+::------------------------------------------------------::
+:: Vérifie si le nom du fichier temporaires existe déjà ::
+::------------------------------------------------------::
+:: Vérifie si le nom du fichier temporaires existe déjà ::
+:nomFichierTemp
+    :: Compte le nombre de fichier présent dans le dossier ::
+    FOR /f %%i IN ('dir "%cd%" /b /a-d') DO SET /a "nbFichier+=1"
+
+    :: Vérifie si le fichier existe déjà ::
+    FOR /l %%C IN (1, 1, %nbFichier%) DO (
+        IF NOT EXIST "temp%%C.txt" (
+            SET "nomFichierTemp%~1=temp%%C.txt"
+            goto :eof
+        )
+    )
+goto :eof
+
+
+
+::---------------------------------------------------------------::
+:: Compte le nombre de dossiers qu'il y a dans le dossier source ::
+::---------------------------------------------------------------::
+:nbDossierInSource
+    FOR /f %%A IN ('dir /b /ad "!pathDossier%~1!"') DO SET /a "nbDossierInSource%~1+=1"
+goto :eof
+
+
+
+
+::---------------------------::
+:: Compare les deux dossiers ::
+::---------------------------::
+:compareDossiers
+    :: Selectionne les deux fichiers à comparer ::
+    FOR /f %%A IN ('dir "!pathDossier1!" /a /b /o:GEN') DO & SET "nbFichier=%%A"
+    (
+        FOR /f %%B IN ('dir "!pathDossier2!" /a /b /o:GEN') DO & SET "nbFichier=%%B" (
+
+        )
+    )
 goto :eof
