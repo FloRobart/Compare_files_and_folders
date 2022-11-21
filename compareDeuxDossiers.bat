48ce6328a7c8da6dd5f5c471683bbc5185f75f72
Floris <Floris.robart11@gmail.com>
2022-11-21 13:09:40 +0100

    update

--- compareDeuxDossiers.bat
+++ compareDeuxDossiers.bat
@@ -1,8 +1,9 @@
 @echo off
 SETLOCAL enabledelayedexpansion
 
-
-
+:: Code éxecuter (main) ::
+FOR /f %%i IN ('dir "%cd%" /a:-d ^| find "fichier"') DO SET "nbFichier=%%i"
 
 ENDLOCAL
+:: Met fin au programme ::
 goto :eof
