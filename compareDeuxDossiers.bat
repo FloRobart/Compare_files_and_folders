420c0ba29a8042b640d58b1d76228a0ebb0c0bf5
Floris <Floris.robart11@gmail.com>
2022-11-25 11:24:48 +0100

    update

--- compareDeuxDossiers.bat
+++ compareDeuxDossiers.bat
@@ -1,46 +1,50 @@
 @echo off
-SETLOCAL enabledelayedexpansion
-
-:: Compte le nombre d'argument passer en paramètre de la fonction ::
-FOR %%i IN (%*) DO SET /a nbArgument+=1
-IF %nbArgument% LSS 2 SET /a "nbArgument=2"
-echo %nbArgument%
-
-FOR %%i IN (%*) DO (
-    SET /a "nbArgument+=1"
-    SET "pathDossier!count!=%%i"
-    echo %%i
-)
-echo %pathDossier1%
-echo %pathDossier2%
-echo %pathDossier3%
-
-::SET "pathDossier1=%~1"
-::SET "pathDossier2=%~2"
-SET "sRet="
 
 ::======================::
 :: Code éxecuter (main) ::
 ::======================::
-FOR /l %%i IN (1, 1, %nbArgument%) DO call :choixDossierSource %%i
-FOR /l %%i IN (1, 1, %nbArgument%) DO call :nomFichierTemp %%i
-FOR /l %%i IN (1, 1, %nbArgument%) DO call :nbDossierInSource %%i
+SETLOCAL enabledelayedexpansion
+    :: Variable ::
+    SET "sRet=msgbox"
 
-FOR /l %%a IN (1, 1, %nbArgument%) DO (
-    FOR /l %%b IN (1, 1, %nbArgument%) DO (
-        IF %%b GTR %%a (
-            IF !nbDossierInSource%%a! EQU !nbDossierInSource%%b! (
-                echo "Le nombre de dossier dans le dossier !pathDossier%%a! est identique au nombre de dossier dans le dossier !pathDossier%%b!"
-            ) else (
-                echo "Le nombre de dossier dans le dossier !pathDossier%%a! est différent du nombre de dossier dans le dossier !pathDossier%%b!"
+
+    :: Compte le nombre d'argument passer en paramètre de la fonction ::
+    FOR %%i IN (%*) DO SET /a nbArgument+=1
+
+    :: Si le nombre d'argument est inférieur à 2, on demande à l'utilisateur combien de dossier il veut comparer ::
+    IF "%nbArgument%" LSS "2" SET /p "nbArgument=Combien de dossier voulez-vous comparer ? : "
+
+    :: Définition des variables %%pathDossier%% ::
+    FOR %%i IN (%*) DO (
+        SET /a "count+=1"
+        SET "pathDossier!count!=%%i"
+    )
+
+    :: Vérifie que les dossiers existe et sont valides ::
+    FOR /l %%i IN (1, 1, %nbArgument%) DO call :choixDossierSource %%i
+    :: Nomme les fichiers temporaires ::
+    ::FOR /l %%i IN (1, 1, %nbArgument%) DO call :nomFichierTemp %%i
+    :: Compte le nombre de dossier présent dans les dossiers sources ::
+    FOR /l %%i IN (1, 1, %nbArgument%) DO call :nbDossierInSource %%i
+
+    :: Compare le nombre de dossier présent dans les dossiers sources ::
+    FOR /l %%a IN (1, 1, %nbArgument%) DO (
+        FOR /l %%b IN (1, 1, %nbArgument%) DO (
+            IF %%b GTR %%a (
+                IF !nbDossierInSource%%a! EQU !nbDossierInSource%%b! (
+                    SET sRet=%sRet% "Il y a %nbDossierInSource%%%a dans le dossier ""!pathDossier%%a!"" et dans le dossier ""!pathDossier%%b!"" ^& vbCRLF ^& "
+                ) else (
+                    SET sRet=%sRet% "Il y a %nbDossierInSource%%%a dans le dossier ""!pathDossier%%a!"" et %nbDossierInSource%%%b dans le dossier ""!pathDossier%%b!"" ^& vbCRLF ^& "
+                )
             )
         )
     )
-)
+
+    :: Afficher le résultat du nombre de dossier dans les dossiers sources ::
+    call :afficherResultat 1
 
 
-
-::call :compareDossiers
+    ::call :compareDossiers
 
 ENDLOCAL
 :: Met fin au programme ::
@@ -75,8 +79,8 @@ goto :eof
 
     :: Vérifie si le fichier existe déjà ::
     FOR /l %%C IN (1, 1, %nbFichier%) DO (
-        IF NOT EXIST "temp%%C.txt" (
-            SET "nomFichierTemp%~1=temp%%C.txt"
+        IF NOT EXIST "temp%%C.%~2" (
+            SET "nomFichierTemp%~1=temp%%C.%~2"
             goto :eof
         )
     )
@@ -90,6 +94,16 @@ goto :eof
 :nbDossierInSource
     SET /a "nbDossierInSource%~1=0"
     FOR /f %%i IN ('dir "!pathDossier%~1!" /b /ad') DO SET /a "nbDossierInSource%~1+=1"
+goto :eof
+
+
+
+::--------------------------------------------------------------------::
+:: Affiche le résultat du nombre de dossier dans les dossiers sources ::
+::--------------------------------------------------------------------::
+:afficherResultat
+    call :nomFichierTemp %~1 "vbs"
+    echo %sRet% > "!nomFichierTemp%~1!"
 goto :eof
 
 
