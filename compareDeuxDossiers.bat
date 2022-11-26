4e659add5bdadfb6e25d88cb5d7ee077db9495de
Floris <Floris.robart11@gmail.com>
2022-11-26 18:59:05 +0100

    tout fonctionnelle sauf l'affichage dans les fichiers temp

--- compareDeuxDossiers.bat
+++ compareDeuxDossiers.bat
@@ -22,18 +22,25 @@ SETLOCAL enabledelayedexpansion
     :: Vérifie que les dossiers existe et sont valides ::
     FOR /l %%i IN (1, 1, %nbArgument%) DO call :choixDossierSource %%i
 
-    :: Nomme les fichiers temporaires ::
-    ::FOR /l %%i IN (1, 1, %nbArgument%) DO call :nomFichierTemp %%i
-
     :: Compte le nombre de dossier présent dans les dossiers sources ::
     FOR /l %%i IN (1, 1, %nbArgument%) DO call :nbDossierInSource %%i
 
-    :: Afficher le résultat du nombre de dossier dans les dossiers sources ::
-    call :afficherNbSousDossier 1
+
+    ::fonction nbSousDossier potentiellement inutile
+    call :nbSousDossier
 
 
-    ::call :compareDossiers
+    :: Affiche le nombre de dossier présent dans les dossiers sources ::
+    call :nomFichierTemp 1 "vbs"
+    echo !sRetNbSousDossier! > !nomFichierTemp1!
+    call !nomFichierTemp1!
 
+
+    :: Compare le contenue des fichiers dans les dossiers sources ::
+    call :compareDossier
+
+
+    call :suppressionFichierTemp
 ENDLOCAL
 :: Met fin au programme ::
 goto :eof
@@ -86,18 +93,25 @@ goto :eof
 
 
 
-::--------------------------------------------------------------------::
-:: Affiche le résultat du nombre de dossier dans les dossiers sources ::
-::--------------------------------------------------------------------::
-:afficherNbSousDossier
-    call :nomFichierTemp %~1 "vbs"
+::---------------------------------------------------------------::
+:: Compte le nombre de fichiers qu'il y a dans le dossier source ::
+::---------------------------------------------------------------::
+:: Inutiliser
+:nbFichierInSource
+    SET /a "nbFichierInSource%~1=0"
+    FOR /f %%i IN ('dir "!pathDossier%~1!" /b /a-d') DO SET /a "nbFichierInSource%~1+=1"
+goto :eof
 
+
+
+::---------------------------------------------------------------::
+:: Détermine le nombre de sous dossier dans les dossiers sources ::
+::---------------------------------------------------------------::
+:nbSousDossier
     FOR /l %%i IN (1, 1, !nbArgument!) DO (
         SET "sRetNbSousDossier=!sRetNbSousDossier! """!pathDossier%%i!"" contient !nbDossierInSource%%i! sous dossiers""
         IF %%i LSS !nbArgument! SET "sRetNbSousDossier=!sRetNbSousDossier! ^& vbCRLF ^&"
     )
-
-    echo !sRetNbSousDossier! > "!nomFichierTemp%~1!"
 goto :eof
 
 
@@ -105,12 +119,115 @@ goto :eof
 ::---------------------------::
 :: Compare les deux dossiers ::
 ::---------------------------::
-:compareDossiers
+:compareDossier
     :: Selectionne les deux fichiers à comparer ::
-    FOR /f %%A IN ('dir "!pathDossier1!" /a /b /o:GEN') DO & SET "nbFichier=%%A"
-    (
-        FOR /f %%B IN ('dir "!pathDossier2!" /a /b /o:GEN') DO & SET "nbFichier=%%B" (
-
+    FOR /f %%A IN ('dir "!pathDossier1!" /a-d /b /o:GEN') DO (
+        FOR /f %%B IN ('dir "!pathDossier2!" /a-d /b /o:GEN') DO (
+            call :compareDeuxFichiers "!pathDossier1!\%%A" "!pathDossier2!\%%B"
         )
     )
+goto :eof
+
+
+
+
+
+
+
+::==============================================::
+:: Fonction qui compare deux fichiers entre eux ::
+::==============================================::
+:compareDeuxFichiers
+    SET "pathFichier1=%~1"
+    SET "pathFichier2=%~2"
+    SET "nbLigneFichier1="
+    SET "nbLigneFichier2="
+
+    SET /a "nbDifferenceFichiers=0"
+
+
+    call :choixFichierSource 1
+    call :choixFichierSource 2
+
+    :: Compte le nombre de ligne dans le fichier 1 ::
+    call :nomFichierTemp 2 "txt"
+    call :nbLigneFichier 1 2
+
+    :: compte le nombre de ligne dans le fichier 2 ::
+    call :nomFichierTemp 3 "txt"
+    call :nbLigneFichier 2 3
+
+
+
+    :: Compare le nombre de ligne des deux fichiers ::
+    SET "msgFichierDiff=msgbox "
+    if "%nbLigneFichier1%" EQU "%nbLigneFichier2%" (
+        :: Compare le contenue des deux fichiers ::
+        call :compareFichiers
+    ) else (
+        SET msgFichierDiff=%msgFichierDiff%"Le nombre de ligne des deux fichiers sont différentes ""%pathFichier1%"" = %nbLigneFichier1% Lignes ""%pathFichier2%"" = %nbLigneFichier2% Lignes"
+    )
+
+    :: Affiche le résultat de la comparaison ::
+    if "%nbDifferenceFichiers%" EQU "0" (
+        ECHO "le contenue des fichiers ""%pathFichier1%"" et ""%pathFichier2%"" sont IDENTIQUES"
+    ) else (
+        echo %msgFichierDiff%"il y a %nbDifferenceFichiers% lignes DIFFERENTES entre les fichiers ""%pathFichier1%"" et ""%pathFichier2%"
+    )
+
+    call :suppressionFichierTemp
+:: Met fin à la comparaison des fichiers passé en paramètre ::
+goto :eof
+
+
+
+:: Vérifie si l'argument est valide ::
+:choixFichierSource
+    IF NOT EXIST "!pathFichier%~1!" (
+        call :nomFichierTemp 4 "vbs"
+        echo msgbox "Un problème est survenue, ""!pathFichier%~1!"" n'existe pas", vbOkOnly+vbCritical, "Erreur" > !nomFichierTemp4!
+        call !nomFichierTemp4!
+        call :suppressionFichierTemp
+        exit /b 1
+    )
+goto :eof
+
+
+
+:: Permet de connaitre le nombre de lignes dans les fichiers ::
+:nbLigneFichier
+    FIND /v /c "" < "!pathFichier%~1!" > "!nomFichierTemp%~2!" 
+    FOR /f "tokens=1 delims=" %%A IN (!nomFichierTemp%~2!) DO SET /a "nbLigneFichier%~1=%%A"
+goto :eof
+
+
+:: Boucle pour lire les deux fichiers ligne par ligne ::
+:compareFichiers
+    call :verifFichier "delims=" 0
+    FOR /l %%C IN (1, 1, !nbLigneFichier1!) DO (
+        call :verifFichier "delims= skip=%%C" %%C
+        SET "rien="
+    )
+goto :eof
+
+
+:: Permet de vérifier si les deux fichiers sont identiques et dans le même ordre ::
+:verifFichier
+    FOR /f "%~1" %%A IN ('type "!pathFichier1!"') DO (
+        FOR /f "%~1" %%B IN ('type "!pathFichier2!"') DO (
+            if NOT "%%A" EQU "%%B" (
+                SET /a "nbDifferenceFichiers+=1"
+            )
+            goto :eof
+        )
+    )
+goto :eof
+
+
+:: Suppressions des fichiers temporaires ::
+:suppressionFichierTemp
+    IF EXIST "!nomFichierTemp1!" del "!nomFichierTemp1!"
+    IF EXIST "!nomFichierTemp2!" del "!nomFichierTemp2!"
+    IF EXIST "!nomFichierTemp3!" del "!nomFichierTemp3!"
+    IF EXIST "!nomFichierTemp4!" del "!nomFichierTemp4!"
 goto :eof
